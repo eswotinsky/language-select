53fc6a414df0143f63322de2de845eb54393049e
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-01-12 15:18:11 -0800

    add custom javascript to show one of two different questions depending on user's response to initial question

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
 $(document).ready(function(){
-  $().submit(function(event){
 
+  //declare variables to hold user responses
+  var q1;
+  var q2;
+  var q3;
+  var q4;
+  var q5;
 
-    event.preventDefault();
+  //Question 1: Creative or Logic?
+  //store user choice in var q1
+  //if user selects "creative", display smVsLg and hide webVsApps
+  //else if user selects "logic", display webVsApps and hide smVsLg
+  $("[name=creativeVsLogic]").change(function() {
+    q1 = $("input:radio[name=creativeVsLogic]:checked").val();
+    if (q1 == "creative") {
+      $("#lgVsSm").show();
+      $("#webVsApps").hide()
+    }
+    else if (q1 == "logic") {
+      $("#lgVsSm").hide();
+      $("#webVsApps").show()
+    }
   })
-});
+
+  //Question 2: Large Company or Small?
+  //store user choice in var q2
+  //regardless of user choice, display uxVsBackend
+  $("[name=lgVsSm]").change(function() {
+    $("uxVsBackend").show()
+  })
+
+
+})
