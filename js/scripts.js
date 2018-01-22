b9e91b9a784b1f650ce5ac36548e086405c2d1f0
Eric Swotinsky <ericswotinsky@gmail.com>
2018-01-22 15:03:29 -0800

    update JS variable names for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,102 +1,102 @@
 $(document).ready(function(){
 
   //declare variables to hold user responses
-  var q1;
-  var q2;
-  var q3;
-  var q4;
-  var q5;
-  var q6;
+  var qCreativeVsLogic;
+  var qLgVsSm;
+  var qWebVsApp;
+  var qUxVsBackend;
+  var qAndVsWin;
+  var qLgVsSm2;
 
   //Question 1: Creative or Logic?
-  //store user choice in var q1
+  //store user choice in var qCreativeVsLogic
   //if user selects "creative", display lgVsSm and hide other questions
   //else if user selects "logic", display webVsApps and hide other questions
   $("[name=creativeVsLogic]").change(function() {
     $("div.results").hide()
-    q1 = $("input:radio[name=creativeVsLogic]:checked").val();
-    if (q1 == "creative") {
+    qCreativeVsLogic = $("input:radio[name=creativeVsLogic]:checked").val();
+    if (qCreativeVsLogic == "creative") {
       $("#lgVsSm").show();
       $("#webVsApps, #lgVsSm2, #uxVsBackend, #androidVsWindows").hide()
     }
-    else if (q1 == "logic") {
+    else if (qCreativeVsLogic == "logic") {
       $("#lgVsSm, #lgVsSm2, #uxVsBackend, #androidVsWindows").hide();
       $("#webVsApps").show()
     }
   })
 
   //Question 2: Large Company or Small?
-  //store user choice in var q2
+  //store user choice in var qLgVsSm
   //regardless of user choice, display uxVsBackend
   $("[name=lgVsSm]").change(function() {
     $("div.results").hide()
-    q2 = $("input:radio[name=lgVsSm]:checked").val();
+    qLgVsSm = $("input:radio[name=lgVsSm]:checked").val();
     $("#webVsApps, #lgVsSm2, #androidVsWindows").hide()
     $("#uxVsBackend").show();
   })
 
   //Question 3: Websites vs Apps?
-  //store user choice in var q3
+  //store user choice in var qWebVsApp
   //if user selects "websites", display lgVsSm2 and hide lower-level questions
   //else if user selects "apps", display androidVsWindows and hide lower-level questions
   $("[name=webVsApps]").change(function() {
     $("div.results").hide()
-    q3 = $("input:radio[name=webVsApps]:checked").val();
-    if (q3 == "websites") {
+    qWebVsApp = $("input:radio[name=webVsApps]:checked").val();
+    if (qWebVsApp == "websites") {
       $("#lgVsSm2").show()
       $("#lgVsSm, #uxVsBackend, #androidVsWindows").hide()
     }
-    else if (q3 == "apps") {
+    else if (qWebVsApp == "apps") {
       $("#lgVsSm, #lgVsSm2, #uxVsBackend").hide()
       $("#androidVsWindows").show()
     }
   })
 
   //Question 4: UX vs Backend?
-  //store user choice in var q4
+  //store user choice in var qUxVsBackend
   //if user selects "ux", display CSS div
-  //else if user selects "backend", AND if user selected "large" for q2, display phpDrupal div
-  //else if user selects "backend", AND if user selected "small" for q2, display rubyRails div
+  //else if user selects "backend", AND if user selected "large" for qLgVsSm, display phpDrupal div
+  //else if user selects "backend", AND if user selected "small" for qLgVsSm, display rubyRails div
     $("[name=uxVsBackend]").change(function() {
       $("div.results").hide()
-      q4 = $("input:radio[name=uxVsBackend]:checked").val();
-      if (q4 == "ux") {
+      qUxVsBackend = $("input:radio[name=uxVsBackend]:checked").val();
+      if (qUxVsBackend == "ux") {
         $("#cssDesign").show();
       }
-      else if (q4 == "backend" && q2 == "large") {
+      else if (qUxVsBackend == "backend" && qLgVsSm == "large") {
         $("#phpDrupal").show();
       }
-      else if (q4 == "backend" && q2 == "small") {
+      else if (qUxVsBackend == "backend" && qLgVsSm == "small") {
         $("div#rubyRails").show();
       }
     })
 
   //Question 5: Android vs Windows?
-  //store user choice in var q5
+  //store user choice in var qAndVsWin
   //if user selects "android", display javaAndroid div
   //else if user selects "windows", display cSharpDotNet div
   $("[name=androidVsWindows]").change(function() {
     $("div.results").hide()
-    q5 = $("input:radio[name=androidVsWindows]:checked").val();
-    if (q5 == "android") {
+    qAndVsWin = $("input:radio[name=androidVsWindows]:checked").val();
+    if (qAndVsWin == "android") {
       $("div#javaAndroid").show();
     }
-    else if (q5 == "windows") {
+    else if (qAndVsWin == "windows") {
       $("div#cSharpDotNet").show();
     }
   })
 
   //Question 6: Large Company or Small v2?
-  //store user choice in var q6
+  //store user choice in var qLgVsSm2
   //if user selects "large", display phpDrupal div
   //else if user selects "small", display rubyRails div
   $("[name=lgVsSm2]").change(function() {
     $("div.results").hide()
-    q6 = $("input:radio[name=lgVsSm2]:checked").val();
-    if (q6 == "large") {
+    qLgVsSm2 = $("input:radio[name=lgVsSm2]:checked").val();
+    if (qLgVsSm2 == "large") {
       $("div#phpDrupal").show();
     }
-    else if (q6 == "small") {
+    else if (qLgVsSm2 == "small") {
       $("div#rubyRails").show();
     }
   })
